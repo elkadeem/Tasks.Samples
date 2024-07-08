cf90ac3949612beefc6a892b4599e33c269f7b35
Wael Elkadim <wael.elkadim@microsoft.com>
2024-07-09 00:17:43 +0300

    Add EF core basics samples

---
+++ EFCore.Basics/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EFCore.Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Department
+    {
+        public string Id { get; set; }
+
+        public string Name { get; private set; }
+        
+    }
+
+    public class Employee
+    {
+        public string Id { get; set; }
+
+        public string Name { get; private set; }
+        
+        public Department Department { get; private set; }
+
+        public Employee? Manager { get; private set; }
+    }
+
+}
