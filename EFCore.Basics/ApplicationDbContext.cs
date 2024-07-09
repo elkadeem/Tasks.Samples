eb80e8634fa0d5427747d6c05dd1fcc48aab6417
Wael Elkadim <wael.elkadim@microsoft.com>
2024-07-09 15:19:10 +0300

    Add DbContext sample

---
+++ EFCore.Basics/ApplicationDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore.Basics
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=AdventureWorks2019;Integrated Security=True;Trust Server Certificate=True");
+            
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductSubcategory> ProductSubcategories { get; set; }
+    }
+
+    [Table("ProductCategory", Schema = "Production")]
+    public class ProductCategory
+    {
+        public int ProductCategoryID { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid Rowguid { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ICollection<ProductSubcategory> ProductSubcategories { get; set; }
+    }
+
+    [Table("ProductSubcategory", Schema = "Production")]
+    public class ProductSubcategory
+    {
+        public int ProductSubcategoryID { get; set; }
+
+        public int ProductCategoryID { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid Rowguid { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ProductCategory ProductCategory { get; set; }
+
+    }
+}
--- EFCore.Basics/Program.cs
+++ EFCore.Basics/Program.cs
@@ -1,30 +1,85 @@
-﻿namespace EFCore.Basics
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace EFCore.Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ApplicationDbContext applicationDbContext = new ApplicationDbContext();
+            Console.WriteLine($"Is SQL: {applicationDbContext.Database.IsSqlServer}" +
+                $" and can connect: {applicationDbContext.Database.CanConnect()}");
+
+            //ProductCategoriesSamples(applicationDbContext);
+
+            //var items = applicationDbContext.Set<ProductSubcategory>().ToList();
+            // Implicitly include ProductCategory
+            //var items = applicationDbContext.ProductSubcategories
+            //    .Include(s => s.ProductCategory)
+            //    .ToList();
+            //foreach (var item in items)
+            //{                
+            //    Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
+            //}
+
+            // Explicitly include ProductCategory
+            //var items2 = applicationDbContext.ProductSubcategories.ToList(); 
+            
+            //foreach (var item in items2)
+            //{
+            //    applicationDbContext
+            //        .Entry(item)
+            //        .Reference(s => s.ProductCategory)
+            //        .Load();
+            //    Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
+            //}
+
+            // Lazy Loading
+            foreach(var item in applicationDbContext.ProductSubcategories.ToList())
+            {
+                Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
+            }
+
+        }
+
+        private static void ProductCategoriesSamples(ApplicationDbContext applicationDbContext)
+        {
+            DisplayProductCategories(applicationDbContext.ProductCategories);
+
+            var orderdCategroies = applicationDbContext
+                .ProductCategories
+                .OrderBy(c => c.Name);
+
+            Console.WriteLine("Ordered categories:");
+            DisplayProductCategories(orderdCategroies);
+
+            var productCategoryItems = applicationDbContext
+                .ProductCategories
+                .Where(c => c.ProductCategoryID >= 1
+                && (c.Name.Contains("c")
+                || c.Name.Contains("A")))
+                .Select(c => new { c.Name })
+                .OrderBy(c => c.Name);
+
+            Console.WriteLine("Ordered categories with select:");
+            foreach (var item in productCategoryItems)
+            {
+                Console.WriteLine($"Category: {item.Name}");
+            }
+
+            var productCategoryCount = applicationDbContext
+                .ProductCategories
+                .Count();
+            Console.WriteLine(productCategoryCount);
+        }
+
+        private static void DisplayProductCategories(IEnumerable<ProductCategory> categories)
+        {
+            foreach (var category in categories)
+            {
+                Console.WriteLine($"Category: {category.Name}");
+            }
         }
     }
-
-    public class Department
-    {
-        public string Id { get; set; }
-
-        public string Name { get; private set; }
-        
-    }
-
-    public class Employee
-    {
-        public string Id { get; set; }
-
-        public string Name { get; private set; }
-        
-        public Department Department { get; private set; }
-
-        public Employee? Manager { get; private set; }
-    }
-
 }
