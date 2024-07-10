04014fe3776fdf41a54202df5279bbe2e66226c9
Wael Elkadim <wael.elkadim@microsoft.com>
2024-07-10 13:13:54 +0300

    CRUD Sample

--- EFCore.Basics/ApplicationDbContext.cs
+++ EFCore.Basics/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
 
 namespace EFCore.Basics
 {
@@ -12,12 +13,23 @@ namespace EFCore.Basics
             optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
             optionsBuilder.EnableSensitiveDataLogging();
 
-            optionsBuilder.UseLazyLoadingProxies();
+            // To Enable lazy loading
+            //optionsBuilder.UseLazyLoadingProxies();
         }
 
         public DbSet<ProductCategory> ProductCategories { get; set; }
 
         public DbSet<ProductSubcategory> ProductSubcategories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<ProductCategory>()
+            //    .HasMany(c => c.ProductSubcategories)
+            //    .WithOne(c => c.ProductCategory)
+            //    .OnDelete(DeleteBehavior.ClientCascade);
+        }
     }
 
     [Table("ProductCategory", Schema = "Production")]
@@ -31,7 +43,7 @@ namespace EFCore.Basics
 
         public DateTime ModifiedDate { get; set; }
 
-        public virtual ICollection<ProductSubcategory> ProductSubcategories { get; set; }
+        public virtual ICollection<ProductSubcategory> ProductSubcategories { get; set; } = new List<ProductSubcategory>();
     }
 
     [Table("ProductSubcategory", Schema = "Production")]
@@ -50,4 +62,62 @@ namespace EFCore.Basics
         public virtual ProductCategory ProductCategory { get; set; }
 
     }
+
+    [Table("Product", Schema = "Production")]
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProductNumber { get; set; }
+
+        public bool MakeFlag { get; set; }
+
+        public bool FinishedGoodsFlag { get; set; }
+
+        public string? Color { get; set; }
+
+        public short SafetyStockLevel { get; set; }
+
+        public short ReorderPoint { get; set; }
+
+        public decimal StandardCost { get; set; }
+
+        public decimal ListPrice { get; set; }
+
+        public string? Size { get; set; }
+
+        public string? SizeUnitMeasureCode { get; set; }
+
+        public string? WeightUnitMeasureCode { get; set; }
+
+        public decimal? Weight { get; set; }
+
+        public int DaysToManufacture { get; set; }
+
+        public string? ProductLine { get; set; }
+
+        public string? Class {  get; set; }
+
+        public string? Style { get; set; }
+
+        
+        public int? ProductSubCategoryId { get; set; }
+
+        public int? ProductModelID { get; set; }
+
+        public DateTime SellStartDate { get; set; }
+
+        public DateTime? SellEndDate { get; set; }
+
+        public DateTime? DiscontinuedDate { get; set; }
+
+        public Guid Rowguid { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public ProductSubcategory ProductSubcategory { get; set; }
+    }
+
 }
--- EFCore.Basics/Program.cs
+++ EFCore.Basics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace EFCore.Basics
@@ -11,36 +12,286 @@ namespace EFCore.Basics
             Console.WriteLine($"Is SQL: {applicationDbContext.Database.IsSqlServer}" +
                 $" and can connect: {applicationDbContext.Database.CanConnect()}");
 
-            //ProductCategoriesSamples(applicationDbContext);
+            var productCategory = new ProductCategory
+            {
+                Name = $"Category 14",
+                ModifiedDate = DateTime.Now,
+                Rowguid = Guid.NewGuid()
+            };
 
-            //var items = applicationDbContext.Set<ProductSubcategory>().ToList();
-            // Implicitly include ProductCategory
-            //var items = applicationDbContext.ProductSubcategories
-            //    .Include(s => s.ProductCategory)
-            //    .ToList();
-            //foreach (var item in items)
-            //{                
-            //    Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
-            //}
+            applicationDbContext.ProductCategories.Add(productCategory);
 
-            // Explicitly include ProductCategory
-            //var items2 = applicationDbContext.ProductSubcategories.ToList(); 
-            
-            //foreach (var item in items2)
-            //{
-            //    applicationDbContext
-            //        .Entry(item)
-            //        .Reference(s => s.ProductCategory)
-            //        .Load();
-            //    Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
-            //}
+            for (int i = 0; i < 10; i++)
+            {
+                var productSubcategory = new ProductSubcategory
+                {
+                    Name = $"Category 14 {i}",
+                    ModifiedDate = DateTime.Now,
+                    Rowguid = Guid.NewGuid(),
+                };
 
-            // Lazy Loading
-            foreach(var item in applicationDbContext.ProductSubcategories.ToList())
+                productCategory.ProductSubcategories.Add(productSubcategory);
+            }
+
+            applicationDbContext.SaveChanges();
+
+            Console.WriteLine("Enter Product Category Id to delete:");
+            foreach (var item in applicationDbContext.ProductCategories.ToList())
+            {
+
+                productCategory.ProductSubcategories.Remove();
+                applicationDbContext.Remove(item);
+                Console.WriteLine($"Id: {item.ProductCategoryID}, Name: {item.Name}");
+            }
+
+            productCategory.ProductSubcategories.Clear();
+            applicationDbContext.ProductCategories.Remove(productCategory);
+
+            applicationDbContext.SaveChanges();
+
+            //AddRelatedProductSubCategory(applicationDbContext);
+
+            //UpdateProductCategoryAndAddOthers(applicationDbContext);
+
+
+
+            //AddUpdateDeleteProudctCategory(applicationDbContext);
+
+            //QueriesSamples(applicationDbContext);
+
+        }
+
+        private static void AddRelatedProductSubCategory(ApplicationDbContext applicationDbContext)
+        {
+            var productCategory = new ProductCategory
+            {
+                Name = $"Category 11",
+                ModifiedDate = DateTime.Now,
+                Rowguid = Guid.NewGuid()
+            };
+
+            applicationDbContext.ProductCategories.Add(productCategory);
+
+            for (int i = 0; i < 10; i++)
+            {
+                var productSubcategory = new ProductSubcategory
+                {
+                    Name = $"Subcategory {i}",
+                    ModifiedDate = DateTime.Now,
+                    Rowguid = Guid.NewGuid(),
+                };
+
+                productCategory.ProductSubcategories.Add(productSubcategory);
+            }
+
+            applicationDbContext.SaveChanges();
+        }
+
+        private static void UpdateProductCategoryAndAddOthers(ApplicationDbContext applicationDbContext)
+        {
+            var productCategory = applicationDbContext.ProductCategories.Find(9);
+            Console.WriteLine($"Category: {productCategory.Name}");
+            productCategory.Name = Console.ReadLine();
+
+            for (int x = 1; x < 10; x++)
+            {
+                productCategory = new ProductCategory
+                {
+                    Name = $"Category {x}",
+                    ModifiedDate = DateTime.Now,
+                    Rowguid = Guid.NewGuid()
+                };
+
+                applicationDbContext.ProductCategories.Add(productCategory);
+            }
+
+            applicationDbContext.SaveChanges();
+        }
+
+        private static void AddUpdateDeleteProudctCategory(ApplicationDbContext applicationDbContext)
+        {
+            ProductCategory productCategory = new ProductCategory()
+            {
+                Name = Console.ReadLine(),
+                ModifiedDate = DateTime.Now,
+                Rowguid = Guid.NewGuid()
+            };
+
+            applicationDbContext.ProductCategories.Add(productCategory);
+
+            var entry = applicationDbContext.Entry(productCategory);
+            Console.WriteLine($"State: {entry.State}");
+
+            applicationDbContext.SaveChanges();
+
+            Console.WriteLine($"Product Category Id: {productCategory.ProductCategoryID}");
+            Console.WriteLine($"State: {entry.State}");
+
+            Console.WriteLine("Update Product Category Name:");
+            productCategory.Name = Console.ReadLine();
+            entry = applicationDbContext.Entry(productCategory);
+            Console.WriteLine($"State: {entry.State}");
+
+            applicationDbContext.SaveChanges();
+
+            Console.WriteLine($"State: {entry.State}");
+
+            Console.WriteLine("Delete Product Category:");
+            applicationDbContext.ProductCategories.Remove(productCategory);
+            entry = applicationDbContext.Entry(productCategory);
+            Console.WriteLine($"State: {entry.State}");
+            applicationDbContext.SaveChanges();
+            Console.WriteLine($"State: {entry.State}");
+        }
+
+        private static void QueriesSamples(ApplicationDbContext applicationDbContext)
+        {
+            ProductCategoriesSamples(applicationDbContext);
+
+            var items = applicationDbContext.Set<ProductSubcategory>().ToList();
+            EagerLoadingSample(applicationDbContext);
+
+            ExplicitLoadingSample(applicationDbContext);
+
+            LazyLoadingSample(applicationDbContext);
+
+            SelectProductsWithPagination(applicationDbContext);
+
+            ProductWithFilter(applicationDbContext);
+
+            ProductWithFilterWithInclude(applicationDbContext);
+
+            FindProduct(applicationDbContext);
+
+            ProductGrouping(applicationDbContext);
+
+            GroupingWithMax(applicationDbContext);
+        }
+
+        private static void GroupingWithMax(ApplicationDbContext applicationDbContext)
+        {
+            applicationDbContext.Products
+                            .GroupBy(c => c.ProductSubcategory.ProductCategory.Name)
+                            .Select(c => new { Category = c.Key, Max = c.Max(c => c.ListPrice) });
+
+            var categoryWithHighestPrice = applicationDbContext.Products
+                 .GroupBy(c => c.ProductSubcategory.ProductCategory.Name)
+                 .Select(c => new { Category = c.Key, MaxProduct = c.OrderByDescending(c => c.ListPrice).FirstOrDefault() });
+
+            foreach (var item in categoryWithHighestPrice)
+            {
+                Console.WriteLine($"Category: {item.Category}, Product: {item.MaxProduct?.Name}, Price: {item.MaxProduct?.ListPrice}");
+            }
+        }
+
+        private static void FindProduct(ApplicationDbContext applicationDbContext)
+        {
+            var product = applicationDbContext.Products.Find(1);
+
+            Console.WriteLine(JsonSerializer.Serialize(product, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        private static void ProductGrouping(ApplicationDbContext applicationDbContext)
+        {
+            var productCategoryProducts = applicationDbContext
+                            .Products
+                            .GroupBy(c => c.ProductSubcategory.ProductCategory.Name)
+                            .Select(c => new { Category = c.Key, Count = c.Count() });
+
+            foreach (var item in productCategoryProducts)
+            {
+                Console.WriteLine($"Category: {item.Category}, Count: {item.Count}");
+            }
+        }
+
+        private static void ProductWithFilterWithInclude(ApplicationDbContext applicationDbContext)
+        {
+            var bikesProductsWithInclude = applicationDbContext.Products
+                            .Include(c => c.ProductSubcategory.ProductCategory)
+                            //.Include(c => c.ProductSubcategory)
+                            //.ThenInclude(c => c.ProductCategory)
+                            .Where(c => c.ProductSubcategory.ProductCategory.Name == "Bikes");
+
+            foreach (var item in bikesProductsWithInclude)
+            {
+                Console.WriteLine($"Product: {item.Name}");
+            }
+        }
+
+        private static void ProductWithFilter(ApplicationDbContext applicationDbContext)
+        {
+            var bikesProducts = applicationDbContext.Products
+                            .Where(c => c.ProductSubcategory.ProductCategory.Name == "Bikes");
+
+            foreach (var item in bikesProducts)
+            {
+                Console.WriteLine($"Product: {item.Name}");
+            }
+        }
+
+        private static void SelectProductsWithPagination(ApplicationDbContext applicationDbContext)
+        {
+            int pageSize = 20;
+            int index = 0;
+            // Skip 0 (index * pageSize) & Take 20 (pageSize)
+            // Skip 20 & Take 20
+            // Skip 40 & Take 20
+            // Last page Total Count / pageSize
+            IQueryable<Product> productList = applicationDbContext.Products;
+
+            productList = productList.Where(c => c.Name.StartsWith("A"));
+            productList = productList.Where(c => c.ListPrice > 0);
+
+            int totalCount = productList.Count();
+            int pageCount = (int)Math.Ceiling((double)totalCount / pageSize);
+
+            for (int i = 0; i < pageCount; i++)
+            {
+                var items = productList
+                    .OrderBy(c => c.Name)
+                    .Skip(i * pageSize).Take(pageSize).ToList();
+                Console.WriteLine($"Page: {i + 1}");
+                foreach (var item in items)
+                {
+                    Console.WriteLine($"Product: {item.Name}");
+                }
+            }
+        }
+
+        private static void EagerLoadingSample(ApplicationDbContext applicationDbContext)
+        {
+            //Eage Loading include ProductCategory
+            var items = applicationDbContext.ProductSubcategories
+                .Include(s => s.ProductCategory)
+                .ToList();
+            foreach (var item in items)
             {
                 Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
             }
+        }
 
+        private static void ExplicitLoadingSample(ApplicationDbContext applicationDbContext)
+        {
+            // Explicitly include ProductCategory
+            var items2 = applicationDbContext.ProductSubcategories.ToList();
+
+            foreach (var item in items2)
+            {
+                applicationDbContext
+                    .Entry(item)
+                    .Reference(s => s.ProductCategory)
+                    .Load();
+                Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
+            }
+        }
+
+        private static void LazyLoadingSample(ApplicationDbContext applicationDbContext)
+        {
+            // Lazy Loading
+            foreach (var item in applicationDbContext.ProductSubcategories.ToList())
+            {
+                Console.WriteLine($"Subcategory: {item.Name}, Category: {item.ProductCategory.Name}");
+            }
         }
 
         private static void ProductCategoriesSamples(ApplicationDbContext applicationDbContext)
