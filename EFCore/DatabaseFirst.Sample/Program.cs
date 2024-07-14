af6caacfd75bf39406aaf24fe7a71d4d0984ea14
Wael Elkadim <wael.elkadim@microsoft.com>
2024-07-14 11:41:44 +0300

    Add database first approache project

---
+++ EFCore/DatabaseFirst.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseFirst.Sample.Model;
+
+namespace DatabaseFirst.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           AdventureWorksDbContext dbContext = new AdventureWorksDbContext();
+
+            var departments = dbContext.Departments.ToList();
+            foreach (var department in departments)
+            {
+                System.Console.WriteLine(department.Name);
+            }
+
+        }
+    }
+}
