422e9c77dbc37f5691d4e2705f72b982b88644b8
Wael Elkadim <wael.elkadim@microsoft.com>
2024-07-15 11:52:28 +0300

    Add Sample DbContext

---
+++ EFCore/CodeFirst.Sample/AnimalsMigrationsWIH/20240715084850_AddAnimals.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirst.Sample.AnimalsMigrationsWIH
+{
+    /// <inheritdoc />
+    public partial class AddAnimals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EducationLevel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Food = table.Column<int>(type: "int", nullable: true),
+                    Vet = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FavoriteToy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Food = table.Column<int>(type: "int", nullable: true),
+                    Vet = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FarmAnimals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Food = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FarmAnimals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Humans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CatId = table.Column<int>(type: "int", nullable: true),
+                    DogId = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Food = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Humans", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Humans_Cats_CatId",
+                        column: x => x.CatId,
+                        principalTable: "Cats",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Humans_Dogs_DogId",
+                        column: x => x.DogId,
+                        principalTable: "Dogs",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Humans_CatId",
+                table: "Humans",
+                column: "CatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Humans_DogId",
+                table: "Humans",
+                column: "DogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FarmAnimals");
+
+            migrationBuilder.DropTable(
+                name: "Humans");
+
+            migrationBuilder.DropTable(
+                name: "Cats");
+
+            migrationBuilder.DropTable(
+                name: "Dogs");
+        }
+    }
+}
---
+++ EFCore/CodeFirst.Sample/Model/AnimalsDbContextWithoutInhertiance.cs
@@ -0,0 +1,58 @@
+﻿using CodeFirst.Sample.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.Sample.Model
+{
+    public class AnimalsDbContextWithoutInhertiance : DbContext
+    {
+        // Current Model Can't be used with Inheritance
+        // Becuase of the following reasons:
+        // 1. The base class is abstract
+        // 2. The derived classes use the base class as a property
+        public AnimalsDbContextWithoutInhertiance()
+        {
+        }
+
+        public DbSet<FarmAnimal> FarmAnimals { get; set; }
+
+        public DbSet<Cat> Cats { get; set; }
+
+        public DbSet<Dog> Dogs { get; set; }
+
+        public DbSet<Human> Humans { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=DbWithoutInhertiance;Integrated Security=True;Trust Server Certificate=True");
+            optionsBuilder.LogTo(System.Console.WriteLine
+                , Microsoft.Extensions.Logging.LogLevel.Information);
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FarmAnimal>()
+                .HasBaseType((Type)null);
+
+            modelBuilder.Entity<Dog>()
+                .HasBaseType((Type)null);
+
+            modelBuilder.Entity<Cat>()
+                .HasBaseType((Type)null);
+
+            modelBuilder.Entity<Human>()
+                .HasBaseType((Type)null);
+
+            modelBuilder.Ignore<Pet>();
+
+            modelBuilder.Ignore<Animal>();
+
+            modelBuilder.Entity<Human>(builder =>
+            {
+                builder.Ignore(c => c.Pets);
+                builder.Ignore(c => c.FavoriteAnimal);
+            });
+        }
+    }
+}
